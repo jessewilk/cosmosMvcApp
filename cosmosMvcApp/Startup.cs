52de314c25e330e54130bbd04b230eb983a9c925
Jesse Wilker <X110019@avidxchange.com>
2020-04-14 11:06:37 -0400

    Adding comments

--- cosmosMvcApp/Startup.cs
+++ cosmosMvcApp/Startup.cs
@@ -13,7 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using todo.Services;
-
+// Adding a test change
 namespace cosmosMvcApp
 {
     public class Startup
